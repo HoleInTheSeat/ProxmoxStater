b56f910c3fd97cd0361b625ee9c0d197882cf1ba
HoleInTheSeat <58385663+HoleInTheSeat@users.noreply.github.com>
2021-02-26 11:42:13 -0600

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+cat /etc/apt/sources.list.d/pve-enterprise.list
+echo
+echo
+echo
+read -p "Press [Enter] key to continue."
+
+cat /etc/default/grub
+echo
+echo
+echo
+read -p "Press [Enter] key to continue."
+
+cat /etc/modules
+echo
+echo
+echo
+read -p "Press [Enter] key to continue."
