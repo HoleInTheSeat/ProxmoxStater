f065ded3982d7ab14f4e359b3380566d040d2c01
HoleInTheSeat <58385663+HoleInTheSeat@users.noreply.github.com>
2021-02-26 10:32:53 -0600

    Update pve_starter.sh

--- pve_starter.sh
+++ pve_starter.sh
@@ -1,1 +1,9 @@
+ #!/bin/bash
 
+#remove subcription warning
+sed -Ezi.bak "s/(Ext.Msg.show\(\{\s+title: gettext\('No valid sub)/void\(\{ \/\/\1/g" /usr/share/javascript/proxmox-widget-toolkit/proxmoxlib.js && systemctl restart pveproxy.service
+
+#remove enterprise apt list
+cp /etc/apt/sources.list.d/pve-enterprise.list /etc/apt/sources.list.d/pve-enterprise.list.bak
+sed '1 s/./#&/' /etc/apt/sources.list.d/pve-enterprise.list >/etc/apt/sources.list.d/pve-enterprise2.list
+mv /etc/apt/sources.list.d/pve-enterprise2.list /etc/apt/sources.list.d/pve-enterprise.list
